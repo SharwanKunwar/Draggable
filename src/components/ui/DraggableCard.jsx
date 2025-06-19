506ebfdd5d19b2d55e619ca87e5b643c7ff4e133
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-19 20:59:02 +0545

    updated for bro

--- src/components/ui/DraggableCard.jsx
+++ src/components/ui/DraggableCard.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import {
   motion,
   useMotionValue,
